e4a8a9e6987e1b012b4c88e5c3d1a00d7cc5dc26
Andrew Olden <Andrew.Olden@gmail.com>
2018-07-17 20:28:24 +0100

    The front page image is now being painted purple

--- RootsSource/App1/MainActivity.cs
+++ RootsSource/App1/MainActivity.cs
@@ -21,14 +21,52 @@ namespace roots
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            im = (ImageView)FindViewById(Resource.Id.myImageView);
+            Android.Graphics.Drawables.BitmapDrawable bd = (Android.Graphics.Drawables.BitmapDrawable)im.Drawable;
+            bmp = bd.Bitmap;
+            var vv11 = ApplyBitmapBrightness(23, bmp);
+            
+            im.SetImageBitmap(vv11);
 
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             return base.OnCreateOptionsMenu(menu);
+        }
+
+        public static Bitmap ApplyBitmapBrightness(int brightnessLevel, Bitmap bitmap)
+        {
+            // Create a temporary bitmap to preserve the original image for the final draw
+            Bitmap brightnessAdjustedBitmap = Bitmap.CreateBitmap(bitmap.Width, bitmap.Height, Bitmap.Config.Argb8888);
+            Canvas c = new Canvas(brightnessAdjustedBitmap);
+            Paint paint = new Paint();
+            ColorMatrix colorMatrix = new ColorMatrix();
+
+            // Apply the brightness filter and draw the final bitmap using 
+            // the paint object and the origin bitmap
+            ColorMatrixColorFilter brightnessFilter = AdjustBrightness(brightnessLevel);
+            paint.SetColorFilter(brightnessFilter);
+            c.DrawBitmap(bitmap, 0, 0, paint);
+            return brightnessAdjustedBitmap;
+        }
+
+        public static ColorMatrixColorFilter AdjustBrightness(int BrightnessLevel)
+        {
+
+            ColorMatrix matrix = new ColorMatrix();
+
+            // This is essentially an identity matrix that adjusts colors based on the fourth 
+            // element of each row of the matrix
+            matrix.Set(new float[] {
+            1F, 0, 0, 0, 0.4F,
+            0, 0, 0, 0,0,
+            0, 0, 1F, 0, 0.4F,
+            0, 0, 0, 0.8F, 0 });
+
+            ColorMatrixColorFilter brightnessFilter = new ColorMatrixColorFilter(matrix);
+            return brightnessFilter;
         }
 
 
